609d7b85ceb3f3094a7a12f64c28bc191a8d784a
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-06-19 22:01:44 +0200

    Add basic filtering by name

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+### RWD tests
+* Chrome DevTools,
+* [Responsive View](http://responsiv.eu/),
+* [Website Responsive Testing Tool](http://responsivetesttool.com/).
--- static/script.js
+++ static/script.js
@@ -1,51 +1,74 @@
 document.addEventListener('DOMContentLoaded', function() {
   //fetching data from json file
-  const getChannels = () => {
-    return fetch('channels.json')
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => console.error(err));
-  };
+  const channels = [];
+  fetch('channels.json')
+    .then(res => res.json())
+    .then(data => channels.push(...data));
 
-  //adding card w channals description
-  const showChannels = async () => {
-    console.log('show');
-    const channelsData = await getChannels();
-    if (!channelsData) return;
-
+  const showChannels = (channelsArr) => {
     const cardsBox = document.querySelector('.js-content');
+    let cardsContent = '';
 
-    channelsData.forEach(el => {
-      const card = document.createElement('div');
-      card.className = 'card';
+    if (!channelsArr.length) {
+      cardsBox.innerHTML = '';
+      return
+    }
 
-      card.innerHTML = `
-        <a class="card__link" href=${el.customUrl} target="_blank">
-          <img class="card__img" src=${el.thumbnails.medium.url} alt="${el.tilte} logo">
-        </a>
-        <h2 class="card__title">${el.title}</h2>
-        <div class="card__desc">
-          <div class="desc__data">
-            <h4 class="desc__name">subscribers:</h4>
-            <p class="desc__value">${el.statistics.subscriberCount}</p>
-          </div>
-          <div class="desc__data">
-            <h4 class="desc__name">videos:</h4>
-            <p class="desc__value">${el.statistics.videoCount}</p>
-          </div>
-          <div class="desc__data">
-            <h4 class="desc__name">views:</h4>
-            <p class="desc__value">${el.statistics.viewCount}</p>
+    channelsArr.forEach(el => {
+      cardsContent += `
+        <div class="card">
+          <a class="card__link" href=${el.customUrl} target="_blank">
+            <img class="card__img" src=${el.thumbnails.medium.url} alt="${el.tilte} logo">
+          </a>
+          <h2 class="card__title">${el.title}</h2>
+          <div class="card__desc">
+            <div class="desc__data">
+              <h4 class="desc__name">subscribers:</h4>
+              <p class="desc__value">${el.statistics.subscriberCount}</p>
+            </div>
+            <div class="desc__data">
+              <h4 class="desc__name">videos:</h4>
+              <p class="desc__value">${el.statistics.videoCount}</p>
+            </div>
+            <div class="desc__data">
+              <h4 class="desc__name">views:</h4>
+              <p class="desc__value">${el.statistics.viewCount}</p>
+            </div>
           </div>
         </div>
       `
-      cardsBox.appendChild(card);
+      cardsBox.innerHTML = cardsContent;
     });
+  };
+
+  //adding all Cards onload
+  const getCardsOnLoad = () => {
+    fetch('channels.json')
+      .then(res => res.json())
+      .then(data => showChannels(data));
+  };
+
+  getCardsOnLoad();
+
+  //searching channels by name
+  const filterInput = document.querySelector('.filter__input');
+
+  const findChannels = (searchWord, channelsArr) => {
+    return channelsArr.filter(channel => {
+        const regex = new RegExp(searchWord, 'gi');
+        return channel.title.match(regex);
+    }) 
+  };
+
+  function matchChannels() {
+    let inputValue = '';
+    if (this.value) {
+      inputValue = this.value;
+    }
+    const matchedChannels = findChannels(inputValue, channels);
+
+    showChannels(matchedChannels);
   }
 
-  showChannels();
-
-  
+  filterInput.addEventListener('keyup', matchChannels);
 });
